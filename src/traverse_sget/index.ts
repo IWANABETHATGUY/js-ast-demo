414128c4ef0a7376eba2f70c4035e37c190ea687
IWANABETHATGUY <974153916@qq.com>
2020-12-10 23:39:24 +0800

    fix: 🐛 types, safe_get  when object is not a object

--- src/traverse_sget/index.ts
+++ src/traverse_sget/index.ts
@@ -1,18 +1,17 @@
-let a = { b: { c: 'something', d: 'ddd', e: {f: 'fff'} }, c: 'test' };
+let a = { b: { c: 'something', d: 'ddd', e: { f: 'fff' } }, c: 'test' };
 
-type key<T> = keyof T;
-type s = key<typeof a>;
-// type Path<T> = T extends object ? `${TT<T> & string}` : '';
-type Path<T> = keyof { [P in keyof T & string as T[P] extends object ? `${P}.${Path<T[P]> & string}` | `${P}` : `${P}`]: P };
+type GetPath<T> = {
+  [P in keyof T & string as T[P] extends object ? `${P}.${Path<T[P]> & string}` | `${P}` : P]: P;
+};
 
-type res = Path<typeof a>;
+type Path<T> = T extends object ?  keyof GetPath<T> : never;
 
-function safe_get<T>(obj: T, ...path: Split<Path<T>>) {
+function safe_get<T>(obj: T, ...path: Split<Path<T>>) {}
+type Split<T> = T extends `${infer R}.${infer U}` ? [R, ...Split<U>] : T extends string ? [T] : [];
 
-}
-type Split<T> = T extends `${infer R}.${infer U}` ? [R, ...Split<U>]  : T extends string ? [T] : [];
+safe_get(a, "b", "e", "f");
+safe_get(a, 'b');
+safe_get(a, 'c');
 
-// @ts-expect-error
-safe_get(a, "b", "d", "f")
-safe_get(a, "b", "e", "f")
-safe_get(a, "c")
+
+safe_get(a.b.c, "");
